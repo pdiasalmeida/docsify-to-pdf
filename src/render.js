476fb2479daf9754dc4dc1e8afb2e890206d01f7
Paulo Dias Almeida <diasalmeida.paulo@gmail.com>
2023-08-31 11:42:53 +0100

    Fix puppeteer freeze

--- src/render.js
+++ src/render.js
@@ -12,6 +12,7 @@ const renderPdf = async ({
   emulateMedia,
 }) => {
   const browser = await puppeteer.launch({
+    args: ['--single-process'],
     defaultViewport: {
       width: 1200,
       height: 1000,
